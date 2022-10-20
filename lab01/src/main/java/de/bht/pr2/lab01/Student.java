38adb05db50764efb70d92096fd49b220c53ec99
tinjurhar <jurhar.tin@gmail.com>
2022-10-20 17:21:11 +0200

    first attempt, fix FVI String[]

--- lab01/src/main/java/de/bht/pr2/lab01/Student.java
+++ lab01/src/main/java/de/bht/pr2/lab01/Student.java
@@ -1,26 +1,78 @@
 package de.bht.pr2.lab01;
 
+import java.util.Arrays;
+
 public class Student {
 
   //-------------------------------------------
   // Attribute
   private String name = "";
+  private int matrikelnummer = 0;
+  private String studiengang = "";
 
-  // Matrikelnummer
-  private int registrationNumber = 0;
+  public static final int RUECKMELDUNG_GEBUEHR = 312;
+  public static final String[] FVI = {"Data Science", "Druck- und Medientechnik", "IT-Sicherheit Online", "Medieninformatik", "Medieninformatik Online", "Screen Based Media", "Technische Informatik - Embedded Systems"};
 
-  // Medieninformatik, Technische Informatik, Druck- und Medientechnik und Screen Based Media
-  private String courseOfStudies = "";
+  //-------------------------------
+  // Konstruktor
+  // Passen Sie den Konstruktor an
+  //-------------------------------
+  public Student(String datenZeile) throws StudentParseException, RegistrationNumberException, WrongCourseOfStudiesException, NotPaidTuitionFeeException{
 
-  // Rückmeldegebühr
-  public static final int TUITION_FEE = 312;
+      String[] inputarr = datenZeile.split(",");
+      if (inputarr.length != 4){
+        System.out.println(datenZeile);
+        throw new StudentParseException(datenZeile);
+      }
 
-  /**
-   * This contructor parses a comma-separated row describing a student object.
-   *
-   * @param dataRow a comma-separated row describing a student object
-   */
-  public Student(String dataRow) {
-    // TODO: Here goes your code ...
+      this.name = inputarr[0];
+      try {
+        this.matrikelnummer = Integer.parseInt(inputarr[1]);
+      } catch(NumberFormatException e){
+        throw new RegistrationNumberException(inputarr[1]);
+    }
+      this.studiengang = inputarr[2];
+      if (!FVI.contains(inputarr[2])){
+        throw new WrongCourseOfStudiesException(inputarr[2]);
+    }
+
+    if (Integer.parseInt(inputarr[3]) < RUECKMELDUNG_GEBUEHR){
+      int diff = RUECKMELDUNG_GEBUEHR-Integer.parseInt(inputarr[3]);
+      throw new NotPaidTuitionFeeException(toString(diff));
+    }
+
+  }
+
+
+  //-------------------------------------------
+  // Methoden
+  public int getMatrikelnummer() {
+    return matrikelnummer;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getStudiengang() {
+    return studiengang;
+  }
+
+  public void setMatrikelnummer(int matrikelnummer) {
+    this.matrikelnummer = matrikelnummer;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setStudiengang(String studiengang) {
+    this.studiengang = studiengang;
+  }
+
+  public String toString() {
+    return "name: " + name
+        + ", matrikelnummer: " + matrikelnummer +
+        ", studiengang:" + studiengang;
   }
 }
