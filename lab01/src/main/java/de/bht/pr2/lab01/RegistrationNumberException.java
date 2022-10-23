b28e3237025a9f89448ec29c9fb0d1323ea0ffa0
tinjurhar <jurhar.tin@gmail.com>
2022-10-23 11:37:31 +0200

    first attempt, fix FVI String[] 2.0

---
+++ lab01/src/main/java/de/bht/pr2/lab01/RegistrationNumberException.java
@@ -0,0 +1,7 @@
+package de.bht.pr2.lab01;
+
+public class RegistrationNumberException extends RuntimeException{
+    public RegistrationNumberException(String message) {
+        super(message);
+    }
+}
